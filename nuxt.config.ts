2be42755b6b75bdd521f51c180751a5f8eb43062
Yohanna Abana <yohanaabana02@gmail.com>
2022-11-06 00:55:03 +0100

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+    modules: ["@nuxtjs/tailwindcss"],
+    tailwindcss: {
+        // add '~tailwind.config` alias
+        exposeConfig: true
+    }
+});
