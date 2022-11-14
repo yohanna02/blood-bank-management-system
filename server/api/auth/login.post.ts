43126e7d992874f2aa18d874dc6dd4827212ac90
Yohanna Abana <yohanaabana02@gmail.com>
2022-11-14 13:47:47 +0100

    added login api

---
+++ server/api/auth/login.post.ts
@@ -0,0 +1,43 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import joi from "joi";
+
+import { createServerError } from "~~/interface/AppError";
+import { AuthI } from "~~/interface/User";
+import prisma from "~~/server/db";
+import { validator } from "~~/server/utils/validator";
+
+const config = useRuntimeConfig();
+
+const loginSchema = joi.object<AuthI>({
+    email: joi.required(),
+    password: joi.required()
+});
+
+export default defineEventHandler(async (event) => {
+    const body = await readBody<AuthI>(event);
+
+    const error = validator(loginSchema, body);
+    if (error) {
+        throw createServerError(422, "Invalid Params", error);
+    }
+
+    const user = await prisma.user.findUnique({ where: { email: body.email } });
+    if (!user) {
+        throw createServerError(404, "Email address not registered");
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(body.password, user.password);
+    if (!isPasswordCorrect) {
+        throw createServerError(422, "Password not correct");
+    }
+
+    const accessToken = jwt.sign({ id: user.id }, config.jwtScecret, { expiresIn: "1h" });
+
+    return {
+        user: {
+            email: user.email
+        },
+        accessToken
+    }
+});
