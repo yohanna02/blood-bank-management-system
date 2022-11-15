f54fc924f22c6907086436719250f7b788b2d6b3
Yohanna Abana <yohanaabana02@gmail.com>
2022-11-15 16:30:02 +0100

    Added a donor interface for typescript

--- interface/Blood.ts
+++ interface/Blood.ts
@@ -1,4 +1,14 @@
+import { DonorSex } from "@prisma/client";
+
 export interface BloodGroupI {
     name: string;
     pintAvailable: number;
 };
+
+export interface DonorI {
+    name: string;
+    bloodGroupId: string;
+    sex: DonorSex;
+    email: string;
+    phoneNumber: string;
+}
