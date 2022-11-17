afc06fad1a6989d7d875c6c6b344f7191b0a6452
Yohanna Abana <yohanaabana02@gmail.com>
2022-11-17 12:21:13 +0100

    added tailwind css container class

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-11/12 mx-auto">
+  <div class="container mx-auto">
     <NuxtLoadingIndicator color="green" />
     <NuxtPage />
   </div>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="flex flex-col sm:flex-row border-b border-slate-300 w-11/12 py-5 justify-between items-center">
+    <nav class="flex flex-col sm:flex-row border-b border-slate-300 py-5 justify-between items-center">
         <h3 class="text-red-500 text-sm sm:text-xl outline-offset-8"><NuxtLink to="/">Blood Bank Management System</NuxtLink></h3>
         <ul class="flex items-center justify-center sm:justify-end gap-3 w-full sm:w-3/4 pt-5 sm:pt-0">
             <li><NuxtLink to="/" class="hover:underline outline-offset-8">Home</NuxtLink></li>
