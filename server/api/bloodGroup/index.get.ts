f1311187fd2d515972ea59565907da8701543dd7
Yohanna Abana <yohanaabana02@gmail.com>
2022-11-20 20:09:29 +0100

    chabged get all bloodgroup api to order by from created date to  updated date

--- server/api/bloodGroup/index.get.ts
+++ server/api/bloodGroup/index.get.ts
@@ -3,7 +3,7 @@ import prisma from "~~/server/db";
 
 export default defineEventHandler(async (event) => {
     if (!event.context.user) throw createServerError(401, "unauthorize");
-    const bloodGroups = await prisma.bloodGroup.findMany({ orderBy: { createdAt: "desc" } });
+    const bloodGroups = await prisma.bloodGroup.findMany({ orderBy: { updatedAt: "desc" } });
 
     return {
         success: true,
